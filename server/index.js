11f2325e851f95172f6e47589a3ceff0b0d6a7ad
Naomi Christabel <naomichristabel91@gmail.com>
2022-05-12 13:36:26 +0530

    path added for deployment server

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express(); 
 const port = process.env.PORT || 3001;
 const http = require("http").Server(app);
+const path = require("path");
 const routes = require('./routes/appointment-routes');
 const connection = require('./db/connection');
 
@@ -17,6 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use('/', routes);
+
+// for deployment
+app.use(express.static(path.resolve(__dirname, "./../client/build")));
+app.get("*", function(request, response) {
+  response.sendFile(path.resolve(__dirname, "./../client/build", "index.html"));
+})
 
 http.listen(port, () => console.log(`Listening on port ${port}`));
 
